9a72dcfa88b6c54e6b8e3d461fac9a4c13982dfa
dawe014 <dawittamiru014@gmail.com>
2024-06-27 22:37:55 +0300

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-""" 
-0-pascal_triangle.py
 """
+returns a pascal triangle
+"""
+
 
 def pascal_triangle(n):
     """
-    Pascal triangle
+    returns a list of integers in a pascal triangle format
     """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return res
+
+    if n <= 0:
+        # return empty list
+        return []
+    pascal = [[1]]
+    if n == 1:
+        return pascal
+
+    for i in range(1, n):
+        left = -1
+        right = 0
+        in_pas = []
+        for j in range(i + 1):
+            num = 0 
+            if left > -1:
+                num += pascal[i - 1][left]
+            if right < i:
+                num += pascal[i - 1][right]
+            left += 1
+            right += 1
+            in_pas.append(num)
+        pascal.append(in_pas)
+    return pascal
